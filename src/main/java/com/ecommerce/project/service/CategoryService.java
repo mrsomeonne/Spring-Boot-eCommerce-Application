93996e1900ff1cd7096ca5d196178d9901248132
Manish Khadka <mrsomeonne@gmail.com>
2024-10-23 21:56:28 +0545

    Delete Category added in Controller

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -7,6 +7,8 @@ import java.util.List;
 public interface CategoryService {
 
     List<Category> getAllCategories();
+
     void createCategory(Category category);
 
+    String deleteCategory(Long categoryId);
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -11,6 +11,8 @@ public class CategoryServiceImpl implements CategoryService {
 
     private List<Category> categories = new ArrayList<>();
 
+    private Long nextId = 1L;
+
 
     @Override
     public List<Category> getAllCategories() {
@@ -19,6 +21,19 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category =
+                categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst().orElse(null);
+        // Check if categoryId is equal to given categoryId
+        if (category == null){
+            return "Category with id {"+ category + "} Not Found";
+        }
+        categories.remove(category);
+        return "Category with CategoryId : "+ categoryId + " deleted successfully";
     }
 }
