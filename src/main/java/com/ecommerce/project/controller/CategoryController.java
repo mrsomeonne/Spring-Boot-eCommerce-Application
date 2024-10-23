98e67b012b5780a0f56188bbe44985acae9c0fe2
Manish Khadka <mrsomeonne@gmail.com>
2024-10-23 20:40:39 +0545

    Added Category Service, Model and Controller

---
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -0,0 +1,33 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private CategoryService categoryService;  //If you put @Autowired it will be Field Injection
+
+    public CategoryController(CategoryService categoryService) { //Constructor Injection
+        this.categoryService = categoryService;
+    }
+
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
+        return "Category Added Successfully";
+    }
+}
---
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -0,0 +1,28 @@
+package com.ecommerce.project.model;
+
+public class Category {
+
+    private Long categoryId;   // ID should be primaryKey and User should not set an ID
+    private String categoryName;
+
+    public Category(Long categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
