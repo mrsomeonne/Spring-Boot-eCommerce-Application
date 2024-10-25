585573b5147ef6b0cdbfef33f629d6140cf29a28
Manish Khadka <mrsomeonne@gmail.com>
2024-10-25 22:40:42 +0545

    Added Repository and Modified code by using Repository

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,9 +1,20 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "category_table")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;   // ID should be primaryKey and User should not set an ID
     private String categoryName;
+
+    public Category() {
+    }
 
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -14,49 +16,75 @@ import java.util.Optional;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private List<Category> categories = new ArrayList<>();
+//    private List<Category> categories = new ArrayList<>();
 
-    private Long nextId = 1L;
+//    private Long nextId = 1L;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+//       return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
+//        category.setCategoryId(nextId++);
+//        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public boolean deleteCategory(Long categoryId) {
-        Category category =
-                categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst().orElse(null);
+//        List<Category> categories = categoryRepository.findAll();
+//        Category category =
+//                categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst().orElse(null);
 
-        // Throw exception directly Inline instead of Boolean true or false
-        //categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst()
-               // .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        // Check if categoryId is equal to given categoryId
-        if (category == null){
+//         Throw exception directly Inline instead of Boolean true or false
+//        categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst()
+//                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+//         Check if categoryId is equal to given categoryId
+
+        Optional<Category> deleteCategoryOpt = categoryRepository.findById(categoryId);
+
+        Category deleteCategory = deleteCategoryOpt.orElseThrow(null);
+
+        if (deleteCategory == null){
             return false;
         }
-        categories.remove(category);
+//        categories.remove(category);
+        categoryRepository.delete(deleteCategory);
         return true;
+
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        Optional<Category> optionalCategory =
-                categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst();
+        Optional<Category> savedCategoryOpt = categoryRepository.findById(categoryId);
 
-        if (optionalCategory.isPresent()){
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
-        }else{
-            throw new  ResponseStatusException(HttpStatus.NOT_FOUND, "Category Not Found");
-        }
+        Category savedCategory = savedCategoryOpt.orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        category.setCategoryId(categoryId);
+        savedCategory  = categoryRepository.save(category);
+        return savedCategory;
+
+
+//        Optional<Category> optionalCategory =
+//                categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst();
+
+//        if (optionalCategory.isPresent()){
+//            Category existingCategory = optionalCategory.get();
+////            existingCategory.setCategoryName(category.getCategoryName());
+////            return existingCategory;
+//
+//            Category updatedCategory = categoryRepository.save(existingCategory);
+//            return updatedCategory;
+//
+//        }else{
+//            throw new  ResponseStatusException(HttpStatus.NOT_FOUND, "Category Not Found");
+//        }
     }
 }
